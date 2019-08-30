cfa894424c9e2c1b378e598db95f016ad5195121
Tim Kaler <tfk@mit.edu>
2019-08-30 04:05:51 +0000

    before commit

--- apps/GCN.C
+++ apps/GCN.C
@@ -1,8 +1,3 @@
-// This code is part of the project "Ligra: A Lightweight Graph Processing
-// Framework for Shared Memory", presented at Principles and Practice of 
-// Parallel Programming, 2013.
-// Copyright (c) 2013 Julian Shun and Guy Blelloch
-//
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
